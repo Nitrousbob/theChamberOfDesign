82c7466877ddc6ac201dd1886a0464e1cb5f431b
BearFulmer <bearfulmer@gmail.com>
2026-03-21 09:45:57 -0700

    added rock paper scissors outline to get started

---
+++ theChamberOfDesign/RPSDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace theChamberOfDesign
+{
+    internal class RPSDisplay
+    {
+        //this class displays the game and runs the menu
+        public void RPSStart()
+        {
+            Console.Title = "Rock, Paper, Scissors";
+            Console.WriteLine("Welcome to Rock, Paper, Scissors.");
+            Console.WriteLine("Player one name: ");
+            string p1Name = Console.ReadLine() ?? "";
+            //create player one
+            RPSPlayer Player1 = new RPSPlayer(p1Name);
+            Console.WriteLine("Player two name: ");
+            string p2Name = Console.ReadLine() ?? "";
+            RPSPlayer Player2 = new RPSPlayer(p2Name);
+            //create player two
+        }
+    }
+}
---
+++ theChamberOfDesign/RPSPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace theChamberOfDesign
+{
+    internal class RPSPlayer
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int Draw {  get; set; }
+
+        public RPSPlayer(string name) 
+        {
+            Name = name;
+            Score = 0;
+            Draw = 0;
+        }
+
+    }
+}
