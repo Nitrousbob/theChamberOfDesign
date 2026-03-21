1480de0437fdc5c78e59a81abf59b962a24533b7
BearFulmer <bearfulmer@gmail.com>
2026-03-21 09:24:05 -0700

    Add project files.

---
+++ theChamberOfDesign/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace theChamberOfDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
